fefe29d24936163250d46597186f7f07cd9fb202
lion695 <keron891@gmail.com>
2026-09-09 14:29:44 +0100

    readies code for deploy

--- twitcher_project/settings.py
+++ twitcher_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY", "fallback-insecure-key-for-local-dev")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = os.getenv('DJANGO_DEBUG', False) == 'True'
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
