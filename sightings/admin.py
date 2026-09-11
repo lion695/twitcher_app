76bf206cee549d62a6a62b7d69c60a45b80ebdb3
lion695 <keron891@gmail.com>
2026-09-11 09:48:42 +0100

    feat: implement customized CommentAdmin layout with bulk approval action

--- sightings/admin.py
+++ sightings/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Sighting
+from .models import Sighting, comment
 
 # Register your models here.
 @admin.register(Sighting)
@@ -19,3 +19,22 @@ class SightingAdmin(admin.ModelAdmin):
     
     # Pre-populates fields or configures entry layouts for streamlined creation
     date_hierarchy = 'date_spotted'
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    """
+    Configures the admin panel interface layout for managing custom Sighting Comments.
+    Includes a custom action to approve pending community content efficiently.
+    """
+    list_display = ('author', 'sighting', 'approved', 'created_on')
+    list_filter = ('approved', 'created_on')
+    search_fields = ('author__username', 'body', 'sighting__species_name')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        """Custom administrative action to bulk approve pending user comments"""
+        queryset.update(approved=True)
+    
+    # Text label that displays in the admin 'Actions' dropdown menu bar
+    approve_comments.short_description = "Approve selected comments"
